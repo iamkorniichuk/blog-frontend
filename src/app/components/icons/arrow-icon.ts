8ac305e5d3febaa39c2a71219d74c5c32a4a062d
iamkorniichuk <iamkorniichuk@gmail.com>
2025-10-02 16:56:37 +0200

    feat: create navbar

--- src/app/components/icons/arrow-icon.ts
+++ src/app/components/icons/arrow-icon.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
+      preserveAspectRatio="xMidYMid meet"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { LogoIconComponent } from '../icons/logo-icon';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [LogoIconComponent],
+  templateUrl: './navbar.html',
+})
+export class NavbarComponent {}
