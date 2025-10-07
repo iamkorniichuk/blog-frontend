bc90ac92e040d326068bc2ebeb10bca2cf468e15
iamkorniichuk <iamkorniichuk@gmail.com>
2025-10-07 22:53:55 +0200

    fix: remove placeholder links from navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 import { LogoIconComponent } from '../icons/logo-icon';
 
 @Component({
   selector: 'app-navbar',
-  imports: [LogoIconComponent],
+  imports: [LogoIconComponent, RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
 })
 export class NavbarComponent {}
